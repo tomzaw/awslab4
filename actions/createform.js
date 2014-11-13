61b5dd205893e982b1aee81922ada28abab12bb4
tomzaw <tomzaw5555@gmail.com>
2014-11-13 15:39:27 +0100

    Update createform.js

--- actions/createform.js
+++ actions/createform.js
@@ -18,10 +18,10 @@ var task = function(request, callback){
 	//3. generate form fields for S3 POST
 	var s3Form = new S3Form(policy);
 	//4. get bucket name
-	var bucket = policy.getConditionValueByKey("buket");
+	var bucketname = policy.getConditionValueByKey("bucket");
 	
 
-	callback(null, {template: INDEX_TEMPLATE, params:{fields:fields, bucket:"WEEIA"}});
+	callback(null, {template: INDEX_TEMPLATE, params:{fields:s3fields, bucket:"WEEIA"}});
 }
 
 exports.action = task;
